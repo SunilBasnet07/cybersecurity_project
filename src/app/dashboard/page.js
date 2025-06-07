aeb227f419f0d783d3cdcfaf1296fc78c9b2903e
Sunilbasnet-max <sunilbasnet4893@gmail.com>
2025-06-07 23:16:37 +0545

    quick change

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -11,9 +11,9 @@ const DashboardPage = () => {
       if(!user){
        router.push(LOGIN_ROUTE)
       }
-      // if(user){
-      //   router.push(DASHBOARD_ROUTE)
-      // }
+      if(user){
+        router.push(DASHBOARD_ROUTE)
+      }
     },[user,router])
   return (
     <div className='mt-16 px-3 py-3'> <p>Hi ! <strong>{user?.name}</strong></p>
